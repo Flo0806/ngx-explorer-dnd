8aa56ed8de6c82f222313be5ad1d6222b8a725ec
Florian Heuberger <fh@flogersoft.de>
2022-12-04 08:16:22 +0100

    add ng-add schematic

--- projects/ngx-explorer-dnd/schematics/ng-add/index.ts
+++ projects/ngx-explorer-dnd/schematics/ng-add/index.ts
@@ -1,5 +1,6 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+//import { addImportToModule } from "@schematics/angular/utility/ast-utils"
 
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
